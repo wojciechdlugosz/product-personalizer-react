10f351b2b4f6a82b54bd4cba679bd0fff0e04404
Wojciech Dlugosz <webmaster@styronet.pl>
2024-08-19 20:06:45 +0200

    Fix personalisation

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,77 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react'
 
 const Product = props => {
+
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  };
+
+  const getPrice = () => {
+    const currentSizePrice = props.sizes.find((size) => size.name === currentSize);
+    return props.basePrice + currentSizePrice.additionalPrice; 
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const selectedProduct = {
+      Name: props.title,
+      Price: getPrice(),
+      Size: currentSize,
+      Color: currentColor,
+    };
+
+    console.log('Summary: ', selectedProduct);
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={props.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map(size =>
+                <li key={size.name}>
+                  <button 
+                    type="button" 
+                    className={clsx(currentSize === size.name && styles.active)}
+                    onClick={e => setCurrentSize(size.name)}>
+                      {size.name}
+                    </button>
+                </li>
+              )}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors.map(color => 
+                <li key={color}>
+                  <button 
+                    type="button" 
+                    className={clsx(prepareColorClassName(color), currentColor === color && styles.active)}
+                    onClick={e => setCurrentColor(color)}>
+                  </button>
+                </li>
+              )}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -41,6 +81,18 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  sizes: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      additionalPrice: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
 
 export default Product;
